a6e5b35c727b6def78fb25c6c958584f86cf0445
JustinasPakalnis <justinas.p@inbox.lt>
2024-09-04 12:03:45 +0300

    click me

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 console.log("labas dienas");
+const allTaskDOMs = document.querySelectorAll(".task");
+
+for (const taskDOM of allTaskDOMs) {
+  const moreDOM = taskDOM.querySelector(".more");
+  const moreActionsDOM = taskDOM.querySelector(".more-actions");
+  if (moreDOM) {
+    moreDOM.addEventListener("click", () => {
+      moreActionsDOM.dataset.visible = true;
+    });
+  }
+}
