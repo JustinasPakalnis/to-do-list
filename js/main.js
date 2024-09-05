7f4ac7a107c266bad1f2814711aa377b85c066e2
JustinasPakalnis <justinas.p@inbox.lt>
2024-09-05 09:24:44 +0300

    asd

--- js/main.js
+++ js/main.js
@@ -1,12 +1,38 @@
-console.log("labas dienas");
+//lightbox
+const newTaskBtnDOM = document.querySelector(".main-header");
+const lightboxDOM = document.querySelector(".lightbox");
+const lightboxCloseDOM = lightboxDOM.querySelector(".close");
+const lightboxBackgroundDOM = lightboxDOM.querySelector(".background");
+newTaskBtnDOM.addEventListener("click", () => {
+  lightboxDOM.dataset.visible = true;
+});
+lightboxCloseDOM.addEventListener("click", () => {
+  lightboxDOM.dataset.visible = false;
+});
+lightboxBackgroundDOM.addEventListener("click", () => {
+  lightboxDOM.dataset.visible = false;
+});
+window.addEventListener("keyup", (e) => {
+  if (e.key === "Escape") {
+    lightboxDOM.dataset.visible = false;
+  }
+});
+//task menu
 const allTaskDOMs = document.querySelectorAll(".task");
+let isMeniuOpen = false;
 
 for (const taskDOM of allTaskDOMs) {
   const moreDOM = taskDOM.querySelector(".more");
   const moreActionsDOM = taskDOM.querySelector(".more-actions");
   if (moreDOM) {
     moreDOM.addEventListener("click", () => {
-      moreActionsDOM.dataset.visible = true;
+      moreActionsDOM.dataset.visible = "true";
     });
   }
 }
+
+window.addEventListener("click", () => {
+  for (const taskDOM of allTaskDOMs) {
+    taskDOM.querySelector(".more-actions").dataset.visible = "false;";
+  }
+});
